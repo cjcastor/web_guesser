6139e1b13b389262318222822edd02076912abcc
cjcastor <cjcastor@protonmail.com>
2017-11-19 23:35:10 +0800

    Added views

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_num = rand(101)
+def check_guess(guess, number)
+  guess = guess.to_i
+  case
+  when guess - 5 > number then return "Way too high!"
+  when guess > number then return "Too high!"
+  when guess + 5 < number then return "Way too low!"
+  when guess < number then return "Too low!"
+  when guess == number then return "You got it!"
+  else return guess
+  end
+end
+
+number = rand(101)
 
 get '/' do
-  "#{random_num} test"
+  guess = params["guess"]
+  message = check_guess(guess, number)
+  erb :index, :locals => {:number => number, :message => message}
 end
